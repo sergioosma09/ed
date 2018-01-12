565a3d8360a7dd162b58c99d7461b8958b1a27b3
sergioosma09 <sergioosma09@gmail.com>
2018-01-12 09:34:44 +0100

    Actualizo el bingo

---
+++ CBingo/CBingo/BomboTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace CBingo
+{
+	[TestFixture]
+	public class BomboTest
+	{
+		[Test]
+		public void SacarBola ()
+		{
+			List<int> bolas = new List<int> ();
+			Bombo bombo = new Bombo ();
+
+			for(int contador=1;contador<=90;contador++){
+				int bola = bombo.SacarBola ();
+				Assert.IsTrue (bola >= 1 && bola <= 90, "Debe estar entre 1 y 90");
+				Assert.IsFalse (bolas.Contains (bola), "bola repetida: " + bola);
+				bolas.Add (bola);
+		}
+	}
+		[Test]
+		public void QuedanBolas(){
+			Bombo bombo = new Bombo ();
+			for (int contador = 1; contador <= 90; contador++) {
+				Assert.IsTrue (bombo.QuedanBolas (), "Deben quedar bolas");
+				bombo.SacarBola ();
+			}
+			Assert.IsFalse (bombo.QuedanBolas (), "No debe quedar ninguna bola");
+}
+
+	}
+}
---
+++ CBingo/CBingo/Panel.cs
@@ -0,0 +1,34 @@
+﻿using Gdk;
+using Gtk;
+using System;
+using System.Collections.Generic;
+
+namespace CBingo
+{
+	
+
+	public class Panel
+	{
+		private static Color COLOR_GREEN = new Color (0, 255, 0);
+		private List<Button> buttons = new List<Button> ();
+		public Panel (Container parent){
+			Table table = new Table (9,10,true);
+			for (int index = 0; index < 90; index++) {
+				Button button = new Button ();
+				button.Label = (index + 1).ToString ();
+				button.Visible = true;
+				uint row = (uint)(index / 10);
+				uint column = (uint)(index % 10);
+				table.Attach (button, column, (uint)(column + 1), row, row + 1);
+				buttons.Add (button);
+			}
+			table.Visible = true;
+			parent.Add (table);
+		}
+		public void MarcarNumero(int numero){
+			Button button = buttons [numero - 1];
+			button.ModifyBg(StateType.Normal, COLOR_GREEN);
+
+		}
+}
+}
